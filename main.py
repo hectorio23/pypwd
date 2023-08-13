efe259f10ede861841a7c94d6c6b279ddb6e6781
hectorio23 <hectorino2789@gmail.com>
2023-08-12 18:55:07 -0600

    0.1 I have been started my password generator

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+import string
+
+
+# This class generate helps to us to generate properties 
+# that allowed to us to save our password, encryp it an so more
+class Generator:
+    def __init__(self, length = 10):
+        self.length = length
+
+    # the main method, makes the perfect password
+    def generate(self, length_password:int = 0) -> str:
+        if length_password == 0: length_password = self.length
+            
+        words = list(string.ascii_lowercase + string.ascii_uppercase)
+        symbols = list(string.punctuation)
+
+        temp_password = str()
+
+        for item in range(length_password):
+            temp_password += words.pop(words.index(random.choice(words)))
+
+            if len(temp_password) == length_password:
+                break
+            
+            if len(symbols) == 31:
+                symbols = list(string.punctuation)
+                words = list(string.ascii_lowercase + string.ascii_uppercase)
+
+            temp_password += symbols.pop(symbols.index(random.choice(symbols)))
+
+
+        return str(temp_password)
+
+
+print(Generator(10000).generate())
+        
