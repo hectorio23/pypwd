c1b3c2ce6190dad0ea25ac7ee6cfabe543ac199e
hectorio23 <hectorino2789@gmail.com>
2023-08-13 14:41:35 -0600

    0.3 Its works

--- commands.py
+++ commands.py
@@ -1,27 +1,58 @@
 # /bin/python3.11
+from src.main import Generator      
+from src import save_pwd
 import argparse
-from src.main import Generator
+import sys
 
+
+# Argument object
 parser = argparse.ArgumentParser(
     prog="pypswd",
     description="Manager of password",
     epilog="Don't with yours passwords  "
 )
 
+parser.add_argument('-p', '--print')
+parser.add_argument('-d', '--delete')
 parser.add_argument('-l'  , '--length', type=int)
 parser.add_argument('-s', '--show')
 parser.add_argument('-sv', '--save')
 
-# ZONE OF VARIABLES
 
+# ZONE OF VARIABLES
 object_collection = parser.parse_args()
 password = ""
 
+# This stantment find out that the user
+# wrote the command show_all and print all the passwords on 
+# the terminal, then, the following commands are not necesaries
+# the program will ignore them
+if object_collection.print:
+    save_pwd.load_data()
+    sys.exit()
 
-
+if item_delete := object_collection.delete:
+    save_pwd.load_data('remove', item_delete)
+    sys.exit()
+# The following zone is the comprobation of the 
+# input user
 if length := object_collection.length:
     password = Generator(length).generate
+
+# Show data by the terminal
 if object_collection.show:
     print(password)
-if object_collection.save:
-    
+
+# Save the data in a binary file 
+# with the sintax key -> value (yes, a dictonary in Python)
+# If the user don't input nothing in this section, it raises an error
+# notice him that he need to specify a user or company
+# for example: <user_account>: <user_password>
+if user := object_collection.save:
+    try:
+        save_pwd.save_data(user, password)
+    except:
+        raise KeyError("Input a <user_account> for the password") from Exception
+
+
+
--- src/save_pwd.py
+++ src/save_pwd.py
@@ -17,17 +17,32 @@ def save_data(company, data):
             data_save = {company:data}
             pickle.dump(data_save, binary_file)
 
-
-def load_data():
+# This function do two thing, read and print the values saved
+# on the file or delete one item from it, the objetive with This
+# function is not repeat code
+def load_data(mode = 'read', item = None):
     try:
+        temp_data = None
         with open('./2038dmf', 'rb') as binary_file:
-            for key, value in pickle.load(binary_file).items():
-                print('\t {} -> {} '.format(key, value))
+            # Just print the values if the mode is on read
+            # In the future I will implement more funcionalities in <mode>
+            if mode == 'read':
+                for key, value in pickle.load(binary_file).items():
+                    print('\t {} ->     {} '.format(key.ljust(12, ' '), value))
+            else:
+
+                # Remove de data storaged in the item (its a key in fact)
+                # if item has something storage 
+                data_from_file = pickle.load(binary_file)
+                if item and (response := data_from_file.pop(item, None)):
+                    temp_data = data_from_file
+                else:
+                    print("Something was wrong, the item tere is not in the collection")
+        if temp_data:
+            with open('./2038dmf', 'wb') as file:
+                pickle.dump(temp_data, file)
+                print("Data deleted successfully")
 
     except Exception as error:
         print(f"Has been ocurred an error: {error}")
-
-save_data("Font Awesome", "Yo soy el desarrolllador mdjeu8384874")
-load_data()
-
 
